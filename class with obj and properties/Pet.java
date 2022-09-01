7db64d92e74dc62aea31e4e3583740fad82b9c73
Wildelyn Arriesgado <111488325+wizarden10@users.noreply.github.com>
2022-09-01 15:32:54 +0800

    Create Pet.java

---
+++ class with obj and properties/Pet.java
@@ -0,0 +1,11 @@
+public class Pet {
+    String name;
+    int trustlevel;
+    int petlevel;
+    boolean ishappy;
+
+    public void sayPetName() {
+        System.out.println(name); 
+    }
+    
+}
