8c3471a6f81c0d2370f571ee2bee2984001e2036
Wildelyn Arriesgado <111488325+wizarden10@users.noreply.github.com>
2022-09-01 15:32:17 +0800

    Create Item.java

---
+++ class with obj and properties/Item.java
@@ -0,0 +1,13 @@
+public class Item {
+    int price;
+    double weight;
+    int quantity;
+    String description;
+    boolean canbetraded;
+
+    public void getdescription(){
+        System.out.println(description);
+    }
+
+    
+}
